ba536bbad05bc81efe99bdcea2308f87a838d8a6
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-14 11:44:15 +0200

    Add expressjs

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+// Serve web pages at localhost:8080/* and fill missing extensions with .html
+app.use(express.static('src/web-pages', {extensions: ['html']}))
+// Server scripts at localhost:8080/scripts and fill missing extensions with .js
+app.use('/scripts', express.static('public/scripts', {extensions: ['js']}))
+// Serve assets at localhost:8080/assets/*
+app.use('/assets', express.static('public/assets'))
+// Following is used for sourceMaps
+app.use('/src/scripts', express.static('src/scripts'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
