1952d7f0bec32204b4ac5cd1f5f4fbc28906ac90
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-15 13:36:15 +0200

    Implement game related mixins

---
+++ src/scripts/utils/WithFixedStepUpdate.ts
@@ -0,0 +1,47 @@
+import { Constructor } from "./ObjectFactory";
+
+export interface IFixedStepUpdate {
+    update: () => void,
+    render: () => void,
+    run: () => void
+};
+
+export const WithFixedStepUpdate = <TBase extends Constructor>(Base: TBase) => {
+    return class extends Base implements IFixedStepUpdate {
+        update = () => { };
+        render = () => { };
+
+        constructor(...args: any[]) {
+            super(...args);
+            this._step = 1 / 60;
+            this._gdt = 0;
+            this._now = 0;
+            this.frame = this.frame.bind(this);
+        }
+
+        public run() {
+            requestAnimationFrame(this.frame);
+        }
+
+        private frame(t: number) {
+            this._now = t;
+            this._last = this._last || (t - this._step * 1000);
+            var dt = Math.min((this._now - this._last) / 1000, 1);
+            this._gdt += dt;
+            while (this._gdt >= dt) {
+                // Update physics at fixed rate
+                this.update();
+                this._gdt -= dt;
+            }
+            // Render frame
+            this.render();
+            this._last = this._now;
+            requestAnimationFrame(this.frame);
+        }
+
+        private _step: number;
+        private _gdt: number;
+        private _now: number
+        private _last?: number;
+    }
+}
---
+++ src/scripts/utils/WithKeyboardHandler.ts
@@ -0,0 +1,48 @@
+import { Constructor } from "./ObjectFactory";
+
+export interface IKeyboardHandler {
+    keys: { up: boolean, right: boolean, left: boolean }
+}
+
+class KeyboardHandler {
+    get up() { return this._up };
+    get left() { return this._left };
+    get right() { return this._right };
+
+    constructor() {
+        this._up = false;
+        this._left = false;
+        this._right = false;
+
+        document.onkeydown = e => {
+            switch (e.key) {
+                case "ArrowLeft": this._left = true; break;
+                case "ArrowRight": this._right = true; break;
+                case "ArrowUp": this._up = true; break;
+            }
+        }
+        document.onkeyup = e => {
+            switch (e.key) {
+                case "ArrowLeft": this._left = false; break;
+                case "ArrowRight": this._right = false; break;
+                case "ArrowUp": this._up = false; break;
+            }
+        }
+
+    }
+
+    private _left: boolean;
+    private _right: boolean;
+    private _up: boolean;
+}
+
+export const WithKeyboardHandler = <TBase extends Constructor>(Base: TBase) => {
+    return class extends Base implements IKeyboardHandler {
+        keys: KeyboardHandler;
+
+        constructor(...args: any[]) {
+            super(...args);
+            this.keys = new KeyboardHandler();
+        }
+    };
+};
