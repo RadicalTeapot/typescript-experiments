8d9d06f2acd6f73bc56eb3debfe5383c1b8db90a
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-16 01:42:18 +0200

    Update Game code to use improved mixins and asset factory

--- src/scripts/simple_platformer/Game.ts
+++ src/scripts/simple_platformer/Game.ts
@@ -1,8 +1,39 @@
 import { WithFixedStepUpdate } from "../utils/WithFixedStepUpdate";
 import { WithKeyboardHandler } from "../utils/WithKeyboardHandler";
+import { AssetLoader, AssetItem } from "../utils/AssetLoader";
+import { GameBaseClass } from "../utils/Constructors";
 
-class GameClass {
-
+class Base implements GameBaseClass {
+    update() {}
+    render() {}
 }
+const BaseConstructor = WithFixedStepUpdate(WithKeyboardHandler(Base));
 
-export const Game = WithFixedStepUpdate(WithKeyboardHandler(GameClass));
+export class Game extends BaseConstructor {
+    constructor() {
+        super();
+        this._assetLoader = new AssetLoader();
+    }
+
+    public setItemsToLoad(...items: AssetItem[]) {
+        this._assetLoader.setItemsToLoad(...items);
+    }
+
+    public update() {
+        if (this.keys.up)
+            console.log("Up pressed");
+    }
+
+    public render() {
+
+    }
+
+    public run() {
+        this._assetLoader
+            .load(() => { console.log("All assets loaded") })
+            .then(result => super.run())
+            .catch(error => console.log(`Error while loading assets: ${error}`));
+    }
+
+    private _assetLoader: AssetLoader;
+}
