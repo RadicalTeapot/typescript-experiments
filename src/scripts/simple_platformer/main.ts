ad67ce24e548d2c2183903056fb8798d308ade8e
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-16 01:42:52 +0200

    Change main to use updated Game class

--- src/scripts/simple_platformer/main.ts
+++ src/scripts/simple_platformer/main.ts
@@ -1,22 +1,21 @@
-import { AssetLoader, AssetType } from "../utils/AssetLoader";
+import { AssetType } from "../utils/AssetLoader";
 import { Game } from "./Game";
 
-const loader = new AssetLoader([
+const game = new Game();
+game.setItemsToLoad(
     ['bgBackground', 'assets/simple_platformer/bg_background.png', AssetType.IMAGE],
     ['bgHills', 'assets/simple_platformer/bg_hills.png', AssetType.IMAGE],
-]);
-
-function allLoaded() {
-    console.log("All assets loaded!");
-}
-
-const game = new Game();
-game.update = function () {
-    if (this.keys.up)
-        console.log("Up key pressed");
-}
-
-loader.load(allLoaded)
-    .then(result => game.run())
-    .catch(error => console.log(`Error while loading assets ${error}`))
-    ;
+    ['bgBackground', 'assets/simple_platformer/bg_background.png', AssetType.IMAGE],
+    ['bgHills', 'assets/simple_platformer/bg_hills.png', AssetType.IMAGE],
+    ['bgTiles', 'assets/simple_platformer/bg_tiles.png', AssetType.IMAGE],
+    ['playerFall', 'assets/simple_platformer/player_fall.png', AssetType.IMAGE],
+    ['playerJump1', 'assets/simple_platformer/player_jump1.png', AssetType.IMAGE],
+    ['playerJump2', 'assets/simple_platformer/player_jump2.png', AssetType.IMAGE],
+    ['playerStand', 'assets/simple_platformer/player_stand.png', AssetType.IMAGE],
+    ['playerWalk1', 'assets/simple_platformer/player_walk1.png', AssetType.IMAGE],
+    ['playerWalk2', 'assets/simple_platformer/player_walk2.png', AssetType.IMAGE],
+    ['playerWalk3', 'assets/simple_platformer/player_walk3.png', AssetType.IMAGE],
+    ['tileFull', 'assets/simple_platformer/tile_full.png', AssetType.IMAGE],
+    ['tileTop', 'assets/simple_platformer/tile_top.png', AssetType.IMAGE],
+);
+game.run();
