8d9c3d4d5992cbd420fc52f28f395df5f84cab8c
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-20 10:57:11 +0200

    Implement render for Grid

--- src/scripts/path-logic/Grid.ts
+++ src/scripts/path-logic/Grid.ts
@@ -1,9 +1,16 @@
+import { AssetType } from "../utils/AssetLoader";
 import { Game } from "./Game";
 
 class Tile {
-    x: number = 0;
-    y: number = 0;
+    x: number;
+    y: number;
     spriteID: string = "";
+
+    constructor(x: number, y: number, spriteID: string) {
+        this.x = x;
+        this.y = y;
+        this.spriteID = spriteID;
+    }
 }
 
 export class Grid {
@@ -11,11 +18,38 @@ export class Grid {
     get height() {return 5};
 
     constructor(game: Game) {
-        this._tiles = [];
         this._game = game;
+        this._tiles = [];
+        let x: number, y: number;
+        for (let i = 0; i < this.width * this.height; i++) {
+            x = i % this.width; y = Math.floor(i/this.width);
+            this._tiles[x + y * this.width] = new Tile(x, y, Math.random() > 0.5 ? 'grass' : 'grassAlt');
+        }
     }
 
-    public render() {}
+    public render() {
+        this._tiles.forEach(tile => {
+            if (this._game.assets.has(AssetType.IMAGE, tile.spriteID)) {
+                this._game.renderer.ctx.drawImage(
+                    this._game.assets.get(AssetType.IMAGE, tile.spriteID),
+                    tile.x * this._game.renderer.tileSize,
+                    tile.y * this._game.renderer.tileSize,
+                    this._game.renderer.tileSize, this._game.renderer.tileSize
+                )
+            }
+        });
+    }
+
+    public getTile(x: number, y: number) {
+        return this._tiles[x + y * this.width];
+    }
+
+    public setTileType(x: number, y: number, tileType: string) {
+        const tile = this.getTile(x, y);
+        if (tile) {
+            tile.spriteID = tileType;
+        }
+    }
 
     private _tiles: Tile[];
     private _game: Game;
