92c1692a0cd6a0bde2904e300b59488528359c6f
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-20 07:17:53 +0200

    Add attach/detach methods to keyboard handler

--- src/scripts/utils/WithKeyboardHandler.ts
+++ src/scripts/utils/WithKeyboardHandler.ts
@@ -13,27 +13,48 @@ class KeyboardHandler {
         this._up = false;
         this._left = false;
         this._right = false;
+        this._isAttached = false;
 
-        document.onkeydown = e => {
-            switch (e.key) {
-                case "ArrowLeft": this._left = true; break;
-                case "ArrowRight": this._right = true; break;
-                case "ArrowUp": this._up = true; break;
-            }
-        }
-        document.onkeyup = e => {
-            switch (e.key) {
-                case "ArrowLeft": this._left = false; break;
-                case "ArrowRight": this._right = false; break;
-                case "ArrowUp": this._up = false; break;
-            }
-        }
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+    }
 
+    public attach() {
+        if (!this._isAttached) {
+            document.addEventListener('keydown', this.handleKeyDown);
+            document.addEventListener('keyup', this.handleKeyUp);
+            this._isAttached = true;
+        }
+    }
+
+    public detach() {
+        if (this._isAttached) {
+            document.removeEventListener('keydown', this.handleKeyDown);
+            document.removeEventListener('keyup', this.handleKeyUp);
+            this._isAttached = false;
+        }
+    }
+
+    private handleKeyDown(e: KeyboardEvent) {
+        switch (e.key) {
+            case "ArrowLeft": this._left = true; break;
+            case "ArrowRight": this._right = true; break;
+            case "ArrowUp": this._up = true; break;
+        }
+    }
+
+    private handleKeyUp(e: KeyboardEvent) {
+        switch (e.key) {
+            case "ArrowLeft": this._left = false; break;
+            case "ArrowRight": this._right = false; break;
+            case "ArrowUp": this._up = false; break;
+        }
     }
 
     private _left: boolean;
     private _right: boolean;
     private _up: boolean;
+    private _isAttached: boolean;
 }
 
 export const WithKeyboardHandler = <TBase extends GameConstructor>(Base: TBase) => {
@@ -43,6 +64,7 @@ export const WithKeyboardHandler = <TBase extends GameConstructor>(Base: TBase) 
         constructor(...args: any[]) {
             super(...args);
             this.keys = new KeyboardHandler();
+            this.keys.attach();
         }
     };
 };
