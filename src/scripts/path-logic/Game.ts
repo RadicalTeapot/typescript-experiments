49bfdc8d1224ace2248186eab3ce365bfc23f6eb
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-21 09:30:46 +0200

    Test implementation of interaction

--- src/scripts/path-logic/Game.ts
+++ src/scripts/path-logic/Game.ts
@@ -21,9 +21,23 @@ export class Game extends BaseConstructor {
         this._assetLoader = new AssetLoader();
         this._renderer = new Renderer(this, canvas);
         this._grid = new Grid(this);
+        this._lastChangeCounter = 0;
     }
 
     public update() {
+        if (this.touchState.isTouching && this._lastChangeCounter === 0) {
+            let [x, y] = this.touchState.lastPosition;
+            x = Math.floor(x / this._renderer.tileSize);
+            y = Math.floor(y / this._renderer.tileSize);
+            const tile = this._grid.getTile(x, y);
+            if (tile) {
+                this._grid.setTileType(x, y, tile.spriteID === "" ? "roadTurnRight" : "");
+            }
+            this._lastChangeCounter = 20;
+        }
+        else if (this._lastChangeCounter > 0) {
+            this._lastChangeCounter--;
+        }
     }
 
     public render() {
@@ -46,4 +60,5 @@ export class Game extends BaseConstructor {
     private _assetLoader: AssetLoader;
     private _renderer: Renderer;
     private _grid: Grid;
+    private _lastChangeCounter: number;
 }
