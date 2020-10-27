4761bfeb4d6db705b74c6a256a9e95af2317ca35
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-27 19:14:16 +0100

    Add level manager

---
+++ src/scripts/path-logic/LevelManager.ts
@@ -0,0 +1,27 @@
+import { TiledMap } from "../utils/TiledJSONParser";
+import { Game } from "./Game";
+import { Level } from "./Level";
+
+export class LevelManager {
+    get currentLevel() { return this._levels[this._currentLevelIndex] }
+
+    constructor(game: Game) {
+        this._game = game;
+    }
+
+    public setMaps(maps: TiledMap[]) {
+        this._levels = maps.map(map => new Level(this._game, map));
+    }
+
+    public startLevel(levelIndex: number) {
+        if (levelIndex < this._levels.length)
+        {
+            this._currentLevelIndex = levelIndex;
+            this._levels[levelIndex].start();
+        }
+    }
+
+    private _game: Game;
+    private _levels: Level[] = [];
+    private _currentLevelIndex = 0;
+}
