7ad59ab02a8ce8d8c0d35279b556db7895b525e1
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-29 11:07:34 +0100

    Update comment in server file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('src/web-pages', {extensions: ['html']}))
 app.use('/scripts', express.static('public/scripts', {extensions: ['js']}))
 // Serve assets at localhost:8080/assets/*
 app.use('/assets', express.static('public/assets'))
-// Following is used for sourceMaps
+// Following is used for sourceMaps in chrome
 app.use('/src/scripts', express.static('src/scripts'))
 
 app.listen(port, () => {
