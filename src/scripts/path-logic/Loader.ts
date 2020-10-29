659131728008c65262d9f02178453b825b6026b3
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-29 08:59:56 +0100

    Move no maps error to loader and add method to load single image

--- src/scripts/path-logic/Loader.ts
+++ src/scripts/path-logic/Loader.ts
@@ -4,10 +4,11 @@ export class Loader {
     public static async loadMaps(pathToMaps: string[]): Promise<TiledMap[]> {
         let maps: TiledMap[] = [];
 
-        if (pathToMaps.length > 0)
-        {
+        if (pathToMaps.length > 0) {
             try { maps = await TiledJSONParser.parseMaps(...pathToMaps); }
             catch (error) { throw new Error(`Error while parsing maps ${error.message}`) }
+        } else {
+            throw new Error("No map to load");
         }
 
         if (!TiledJSONParser.areTilesetsOrderIdentical(maps))
@@ -23,5 +24,17 @@ export class Loader {
         catch (error) { throw new Error(`Error while parsing tilesets ${error.message}`) }
 
         return tiles;
+    }
+
+    public static async loadImage(path: string): Promise<HTMLImageElement> {
+        const image = new Promise<HTMLImageElement>((resolve, reject) => {
+            let image = new Image();
+            image.onload = () => resolve(image);
+            image.onerror = () => reject(`Could not load image at path ${path}`);
+            image.src = path;
+        });
+
+        try { return image; }
+        catch (error) { throw new Error(error); }
     }
 }
